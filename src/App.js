e20dbec2eb9d4273d2ff20180288cd057c6e1180
iwan kruger <iwankruger@gmail.com>
2020-08-09 13:12:16 +0200

    Added eslint
    npm install eslint --save-dev
    npm install eslint-config-rallycoding --save-dev

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 
+
 class App extends Component {
+
 
     componentWillMount() {
         // Initialise stuff here
