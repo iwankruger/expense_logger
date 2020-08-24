58d47240284a5f7c01cd17fea3cae90689c5b12e
iwan kruger <iwankruger@gmail.com>
2020-08-24 23:18:12 +0200

    Get transactions from backend

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -17,6 +17,7 @@ import {
   } from 'amazon-cognito-identity-js';
 import * as authConfig from '../../authConfig';
 import { Actions } from 'react-native-router-flux';
+import Moment from 'moment';
 
 const POOL_DATA = {
     UserPoolId: authConfig.cognitoConfig.userPoolId, 
@@ -70,11 +71,18 @@ export const loginUser = ({ email, password }) => {
                 console.log(result);
                 console.log(result.idToken.jwtToken);
 
-                AsyncStorage.setItem('loginToken', result.idToken.jwtToken).then(() => {
+                const loginData = {
+                    login: email, 
+                    loginToken: result.idToken.jwtToken
+                };
+                console.log(loginData);
+
+                AsyncStorage.setItem('loginData', JSON.stringify(loginData)).then(() => {
                     console.log('SAVED IP IN SESSION ');
                     dispatch({ type: LOGIN_USER_SUCCESS, payload: 'user' });
                     Actions.main();
-                }).catch(() => {
+                }).catch((e) => {
+                    console.log('EEE ',e);
                     dispatch({ type: LOGIN_USER_FAIL });   
                 });     
 
@@ -95,13 +103,23 @@ export const checkIfUserIsLoggedIn = () => {
 
     return async (dispatch) => {
        
-        const loginToken = await AsyncStorage.getItem('loginToken');
-        console.log('login token ', loginToken);
+        let loginData = await AsyncStorage.getItem('loginData');
+        loginData = JSON.parse(loginData);
+
+        const loginToken = loginData.loginToken;
+        const login = loginData.login;
+       
+        // set begin and end date of current month
+        const date = new Date();
+        let dateBegin = new Date(date.getFullYear(), date.getMonth(), 1);
+        let dateEnd = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+        dateBegin = Moment(dateBegin).format('YYYY-MM-DD');
+        dateEnd = Moment(dateEnd).format('YYYY-MM-DD');
 
         const categories = await getCategories(loginToken);
         console.log('categories ', categories);
 
-        const transactions = await getTransactions(loginToken);
+        const transactions = await getTransactions(loginToken, login, dateBegin, dateEnd);
         console.log('transactions ', transactions);
         
         const userData = getUserData(categories, transactions);
@@ -115,7 +133,7 @@ export const checkIfUserIsLoggedIn = () => {
 const getCategories = (loginToken) => {
     return axios.get(`https://mbvhmpnj3c.execute-api.us-east-1.amazonaws.com/dev/categories`,{
         headers: {
-          'Authorization': loginToken
+          Authorization: loginToken
         }
       }).then((categories) => { 
         return categories.data;
@@ -124,13 +142,14 @@ const getCategories = (loginToken) => {
     });       
 };
 
-const getTransactions = (loginToken) => {
-    return axios.get(`https://mbvhmpnj3c.execute-api.us-east-1.amazonaws.com/dev/transactions`,{
+const getTransactions = (loginToken, login, dateBegin, dateEnd) => {
+    console.log(`https://mbvhmpnj3c.execute-api.us-east-1.amazonaws.com/dev/transactions?userId=${login}&dateBegin=${dateBegin}&dateEnd=${dateEnd}`);
+    return axios.get(`https://mbvhmpnj3c.execute-api.us-east-1.amazonaws.com/dev/transactions?userId=${login}&dateBegin=${dateBegin}&dateEnd=${dateEnd}`,{
         headers: {
-          'Authorization': loginToken
+          Authorization: loginToken
         }
       }).then((stations) => {
-        console.log('GET  stations result ', stations);
+        console.log('GET  transactions result ', stations);
         
         return stations.data;
     }).catch((error) => {
@@ -142,12 +161,21 @@ const getTransactions = (loginToken) => {
 const getUserData = (categories, transactions) => {
 
     const data = [];
-    console.log('CAT ',categories.length);
-    console.log('CAT ',categories);
+    console.log('CAT ', categories.length);
+    console.log('CAT ', categories);
+
+    const transactionTotals = {};
+    for (let i = 0; i < transactions.length; i++) {
+        let total = 0;
+        if (transactionTotals[transactions[i].categoryId]) {
+            total = transactionTotals[transactions[i].categoryId];
+        }
+        transactionTotals[transactions[i].categoryId] = total + transactions[i].value;
+    }
 
     for (let i = 0; i < categories.length; i++) {
-        const remaining = 100;
-        const total = 700;
+        const total = transactionTotals[categories[i].categoryId] ? transactionTotals[categories[i].categoryId] : 0;
+        const remaining = categories[i].budget - total;
         data.push({ ...categories[i], remaining, total });
     }
     console.log(data);
